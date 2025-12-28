901175d591f01ade4a2657838008daa267555292
Wkrisi <kristiqnpetrov650@gmail.com>
2025-12-28 11:40:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,28 +4,30 @@ if [ -f /etc/NIXOS ] && ! command -v dialog &> /dev/null; then
     exec nix-shell -p dialog --run "$(printf "%q " "$0" "$@")"
 fi
 
-check_dependencies() {
-    if ! command -v dialog &> /dev/null; then
-        if [ -f /etc/arch-release ]; then
-            sudo pacman -S --noconfirm dialog
-            sudo pacman -S wlogout
-            sudo pacman -S swww
-            sudo pacman -S waybar
-            sudo pacman -S hyprland
-        else
-            exit 1
+# Дефинираме функцията, но ще я извикаме по-късно
+install_packages() {
+    if [ -f /etc/arch-release ]; then
+        echo "Updating system and installing base dependencies..."
+        sudo pacman -S --noconfirm dialog wlogout swww waybar hyprland
+
+        if [[ $SELECTED == *"Hyprland"* ]]; then
+            echo "Configuring Hyprland..."
+            sudo pacman -S --noconfirm hyprland
+        fi
+        
+        if [[ $SELECTED == *"Waybar"* ]]; then
+            echo "Configuring Waybar..."
+            sudo pacman -S --noconfirm waybar
         fi
     fi
 }
-
-check_dependencies
 
 BACKTITLE="Krisi's Dotfiles Installer"
 HEIGHT=15
 WIDTH=65
 
 dialog --backtitle "$BACKTITLE" \
---title " Welcome! Hi I'm Kristiyan " \
+--title " Welcome! " \
 --yesno "Hi! I'm Kristiyan. This is the project I've been working on.\n\n                  Do you want to continue?" \
 $HEIGHT $WIDTH
 
@@ -35,15 +37,14 @@ if [ $? -ne 0 ]; then
 fi
 
 TEMP_FILE=$(mktemp)
+
+# Поправен checklist блок
 dialog --backtitle "$BACKTITLE" \
 --title " Selection " \
 --checklist "Select which configs to install:" $HEIGHT $WIDTH 10 \
 "Hyprland" "Window Manager config" ON \
 "Waybar" "Status bar theme" ON \
-"Kitty" "Terminal configuration" ON \
-"SwayNC" "Notification center" ON \
-"Zsh" "Shell configuration (.zshrc)" OFF \
-"Themes" "Vimix & Catppuccin themes" OFF 2> $TEMP_FILE
+"Zsh" "Shell configuration (.zshrc)" OFF 2> $TEMP_FILE
 
 if [ $? -ne 0 ]; then
     rm $TEMP_FILE
@@ -51,15 +52,24 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-clear
-exit 0
 SELECTED=$(cat $TEMP_FILE)
 rm $TEMP_FILE
 
 clear
+
+# Първо проверяваме дали изобщо имаме dialog (за всеки случай)
+if ! command -v dialog &> /dev/null && [ ! -f /etc/NIXOS ]; then
+    if [ -f /etc/arch-release ]; then
+        sudo pacman -S --noconfirm dialog
+    fi
+fi
+
+# Извикваме инсталацията СЛЕД като вече знаем какво е избрано
+install_packages
+
 echo "Installing selected components: $SELECTED"
 
-# Тук добави реалните си команди, например:
+# Тук можеш да сложиш твоя git clone
 # git clone https://github.com/Wkrisi/my-configuration-files.git
 
 dialog --backtitle "$BACKTITLE" \
